bc74aa7f76aeb68d4c8b456bd14ff35ff8c4a763
Ben Davenport-Ray <ben@localhost.localdomain>
2015-07-13 15:52:02 -0400

    Added my custom zsh files and theme. Wooo!

---
+++ custom/aliases.zsh
@@ -0,0 +1,34 @@
+# My massive alias file. Viewer beware.
+# Todo.txt aliases.
+alias t="todo.sh"
+alias ta="t add"
+alias tl="clear; t ls"
+alias td="t do"
+
+
+# Environment aliases.
+alias ea="vim ~/dotfiles/oh-my-zsh/custom/aliases.zsh"
+function aa {
+    echo "alias $1='$2'" >> ~/dotfiles/oh-my-zsh/custom/aliases.zsh
+    source ~/.zshrc
+}
+alias ei3="vim ~/dotfiles/i3/config"
+alias reload="source ~/.zshrc"
+
+# Ease of use aliases.
+alias ping="ping -c 3"
+alias ls="ls --color -xX"
+alias eclimd="/usr/share/eclipse/eclimd &"
+
+# Tmux aliases
+alias tmn="tmux new-session -s -A "
+
+## Make this a function. Wheeeee!
+alias pomo="sleep 25m && printf '\apomodoro done!\n'"
+
+# usage: ghclone laserswald todoc
+ghc(){
+    git clone http://github.com/$1/$2.git
+}
+
+
